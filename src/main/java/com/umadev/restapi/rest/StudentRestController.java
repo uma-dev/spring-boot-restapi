70931da3c808054a962b8925bc62ebd2d6f4a07b
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-19 14:38:29 -0600

    Added Exception Handler in REST service

--- src/main/java/com/umadev/restapi/rest/StudentRestController.java
+++ src/main/java/com/umadev/restapi/rest/StudentRestController.java
@@ -3,6 +3,9 @@ package com.umadev.restapi.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +45,26 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent ( @PathVariable int studentId){
         // just index of list, can be a database. 
+
+        // Check index in range
+        if(studentId < 0 || studentId > theStudents.size()){
+            throw new StudentNotFoundException("Student ID not found - " + studentId);
+        }
         return theStudents.get(studentId);
     }
+
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc ){
+        // Create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // Return ResponseEntity
+        // Jackson will convert the POJO in JSON
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND );
+    } 
+
 }
